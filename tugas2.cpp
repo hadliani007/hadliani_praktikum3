4d34c2531e6fd3e65e3f1b609dd8fbe287829635
hadliani007 <32351403+hadliani007@users.noreply.github.com>
2017-11-07 17:58:36 +0800

    menghitung luas persegi panjang

--- tugas2.cpp
+++ tugas2.cpp
@@ -6,4 +6,13 @@ using namespace std;
 int main () {
 char bidang;
 double p,l,a,t,L;
+	if (bidang == 'P' || bidang == 'p') {	
+		cout << "   Mengitung luas persegi panjang \n";	
+		cout << "   Masukkan panjangnya : ";
+		cin >> p;
+		cout << "   Masukkan lebarnya   : ";
+		cin >> l;
+		L = p*l;
+		cout << "   Luas persegi panjang : "<< L<<endl;
+	}
 }
